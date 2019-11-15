d8cbf60c941d6c5e8ec649a3e69514ee4268e0d8
yujun858 <yujun.hz@qq.com>
2019-11-16 07:35:51 +0800

    namedtuple enumerate collections

--- day24_1106_python/README.md
+++ day24_1106_python/README.md
@@ -6,4 +6,14 @@
 - enumerate 
     - 跟zip功能比较像
     - 对可迭代对象里每一个元素，配上一个索引，然后索引内容构成tuple类型;
+## collections模块
+- namedtuple
+    - tuple : 可以命名tuple
+- dequeue
+    - 比较方便解决频繁删除插入带来的效率问题
+
+- defaultdict
+    - 当直接读取dict不存在属性时候，直接返回默认值
+- Counter
+    - 统计字符串的个数
     
--- day24_1106_python/example01.py
+++ day24_1106_python/example01.py
@@ -1,10 +1,30 @@
 # print('hello world')
-l1 = [1,2,3,4,5]
-l2 = [11,22,33,44,55]
-z = zip(l1,l2)
+import collections
+l1 = [1, 2, 3, 4, 5]
+l2 = [11, 22, 33, 44, 55]
+z = zip(l1, l2)
 print(type(z))
 l3 = [i for i in z]
 print(l3)
-e = enumerate(l1,start=1)
+e = enumerate(l1, start=1)
 l4 = [i for i in e]
-print(l4) #[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
+print(l4)  # [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
+Point = collections.namedtuple('point', ['x', 'y'])
+p = Point(11, 22)
+print(p.x)
+print(p[0])
+Circle = collections.namedtuple('Circle', ['x', 'y', 'r'])
+c = Circle(100, 150, 50)
+q = collections.deque(['a', 'b', 'c'])
+q.append('d')
+q.appendleft('0')
+print(q)
+d = {'one': 1, 'two': 2}
+# print('three')-->抛出异常
+print(d['one'])
+def func(): return '刘大拿'
+d2 = collections.defaultdict(func)
+print(d2['two'])
+print(d2['one'])
+c = collections.Counter('dfdgdgdfafrdsfdsfdjsfjdskjlfhdaoufjhvsaldalsfd')
+print(c)
