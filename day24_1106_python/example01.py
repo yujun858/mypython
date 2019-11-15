48d24d50a78258121f085a304a877aa94e466ad2
yujun858 <yujun.hz@qq.com>
2019-11-15 19:43:43 +0800

    enumerate

--- day24_1106_python/example01.py
+++ day24_1106_python/example01.py
@@ -3,5 +3,8 @@ l1 = [1,2,3,4,5]
 l2 = [11,22,33,44,55]
 z = zip(l1,l2)
 print(type(z))
-for i in z:
-    print(i)
+l3 = [i for i in z]
+print(l3)
+e = enumerate(l1,start=1)
+l4 = [i for i in e]
+print(l4) #[(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
